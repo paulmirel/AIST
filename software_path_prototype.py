c064e00f34370225959ca8881a486c405551d101
paulmirel <paul.mirel@startmail.com>
2025-12-18 11:28:44 -0500

    demonstration of arbitrary field correction

--- software_path_prototype.py
+++ software_path_prototype.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 ar1 = np.random.rand(410, 410, 10)
 ar2 = np.empty_like(ar1)
@@ -17,9 +18,24 @@ for n in range (0, z_count):
 
 ars = np.unstack( ar2, axis = 2) 
 
-r = SQRT((xn - x’)^2 + (yn - y’)^2 ) 
+def get_r( x, y ):
+    x_center = 200
+    y_center = 208
+    return math.sqrt((x - x_center)**2 + (y - y_center)**2 ) 
 
-plt.imshow(ars[0], cmap='gray')
+if False: 
+    plt.imshow(ars[0], cmap='gray')
+    plt.colorbar()
+    plt.show()
+
+flar1 = np.empty([x_count, y_count])
+
+for x in range(0, x_count):
+	for y in range (0, y_count):
+            value = ars[0][ x, y ]
+            radius = get_r(x,y)
+            flar1[x, y,] = value/radius**4 + value * math.sqrt(radius) - 417 * radius
+
+plt.imshow(flar1, cmap='gray')
 plt.colorbar()
 plt.show()
-
