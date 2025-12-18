b6974c46a15223f8757605e87af67ad5bf087033
Paul Mirel <paul.mirel@startmail.com>
2025-12-18 09:53:51 -0500

    converting to binary

--- software_path_prototype.py
+++ software_path_prototype.py
@@ -8,7 +8,7 @@ random_array = numpy.random.rand(40, 40, 10)
 nonzero_random_array = numpy.empty_like(random_array)
 
 def make_nonzero( value ): 
-    return int(65535*(value+0.1)/10)
+    return bin(int(65535*(value+0.1)/10))
 
 x_count, y_count, z_count = random_array.shape
 for n in range (0, z_count):
@@ -18,7 +18,7 @@ for n in range (0, z_count):
 
 image_arrays = numpy.unstack( nonzero_random_array, axis = 2 )
 print(image_arrays[0])
-print(image_arrays[0][0,1])
+
 
 if False: 
     images = []
