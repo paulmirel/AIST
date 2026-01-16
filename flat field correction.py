ed884a85e66dbe7414cae35feeb4f23a09ec7f7f
Paul Mirel <paul.mirel@startmail.com>
2026-01-16 13:31:40 -0500

    picking out central pixels

--- flat field correction.py
+++ flat field correction.py
@@ -25,18 +25,26 @@ one_array_per_band = np.unstack(data_cube_array, axis = 2)
 
 with open('../AIST_data_files/Cubert_Ultris_VNIR_spectral_bands.csv', newline='') as csv_band_list:
     band_tuples = list(csv.reader(csv_band_list))
-
+print(band_tuples[0][0]+", "+band_tuples[0][1])
 print(band_tuples[1][0], band_tuples[1][1])
 
+center_pixels_radius = 20 
+pixel_count = 410
+center_address = pixel_count/2
 
-
+center_average_values = []
 for index in range (0, len(one_array_per_band)): 
-    print("showing band:", index)
-    plt.imshow(one_array_per_band[index], cmap='gray')
-    plt.title('Wavelength ' + str(band_tuples[index+1][1])+ "nm")
-    plt.colorbar()
-    plt.figure(1)
-    plt.show()
-    plt.pause(1)
-    plt.clf()
-       
+    center_pixels_count = 0 
+    value_accumulator = 0 
+    for x in range(0, pixel_count):
+        for y in range(0, pixel_count):
+            #if x**2 + y**2 <= center_pixels_radius**2: #from the corner!!
+            if (x-center_address)**2 + (y-center_address)**2 <= center_pixels_radius**2:
+                center_pixels_count+=1
+                value_accumulator += one_array_per_band[index][x][y]
+
+    #print( "radius =", center_pixels_radius )
+    #print( "pixels in center =", center_pixels_count )
+    #print( "expected pixels =", 3.14*center_pixels_radius**2)
+    center_average_values.append(value_accumulator/center_pixels_count)
+    print( "wavelength", band_tuples[index+1][1],"nm", "average value =", center_average_values[index])
