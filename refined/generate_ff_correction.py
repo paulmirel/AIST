d861372257624417110f55902104d310c7440ecb
Paul Mirel <paul.mirel@startmail.com>
2026-01-21 13:55:26 -0500

    check for saturation

--- refined/generate_ff_correction.py
+++ refined/generate_ff_correction.py
@@ -9,3 +9,10 @@ print(input_cube.shape)
 if input_cube.shape == (410,410,164): print("data input is the correct shape")
 else: print("data input failed: wrong shape")
 print(input_cube.dtype)
+
+# check for saturation
+max_possible = 65535 #unit16
+max_value = np.max(input_cube)
+dynamic_range_fraction = round(max_value/max_possible,3)
+if max_value > max_possible - 1: print( "Data contains saturated pixels. Select a different input file.")
+else: print("dynamic_range_fraction =", dynamic_range_fraction)
