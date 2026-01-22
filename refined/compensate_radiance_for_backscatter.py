f2fb556058c904c9a14b68d1430e71ba4f51436f
Paul Mirel <paul.mirel@startmail.com>
2026-01-22 17:03:03 -0500

    begin radiance adjust

--- refined/compensate_radiance_for_backscatter.py
+++ refined/compensate_radiance_for_backscatter.py
@@ -1,7 +1,17 @@
 import numpy as np
+from pathlib import Path
 
-
+print("Ultris bandwidth is 10nm FWHM for each channel")
+print("What is the bandwidth of the Ultris, and of the ASD?")
 # import the asd files, direct view and monitor
 # average them
 # compensate for backscatter
 # save compensated files
+input_folder = '../AIST_data_files/paul_use_these_CASALS_calibration/ASD with Direct View of Sphere (treated as unloaded sphere)/'
+input_filename = 'CASALS_1L_DirectView00000.asd.txt'
+with open(Path(input_folder, input_filename), 'r') as fp:
+    x = fp.readlines()[690:695]#[40:691]
+    print(x)
+
+
+## figure out a method to combine bands, get it workiing, and then run it past Jesse
