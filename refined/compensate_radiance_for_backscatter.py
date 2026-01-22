96c2cf34088eef5861e34c388b10cc28761906ab
Paul Mirel <paul.mirel@startmail.com>
2026-01-22 09:09:13 -0500

    begin radiance calibration codes

---
+++ refined/compensate_radiance_for_backscatter.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+# import the asd files, direct view and monitor
+# average them
+# compensate for backscatter
+# save compensated files
---
+++ refined/raw_to_radiance.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# import backscatter compensated radiance values
+# import the raw data cube, after flat field calibration, 
+# and after wavelength calibration (if available)
+
+# how to we cope with bandwidth mismatch? Sum? 
+# process values, pixel by pixel, by wavelength to radiance values
+# units? W/cm^2/nm/sr
