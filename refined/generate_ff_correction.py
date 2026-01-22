c27720753d8133ccc3b25c3883a10f656b2ecd0d
Paul Mirel <paul.mirel@startmail.com>
2026-01-22 06:12:48 -0500

    completed gen ff correction

--- refined/generate_ff_correction.py
+++ refined/generate_ff_correction.py
@@ -23,10 +23,10 @@ def main():
     #print(wavelengths_nm)
 
     # find the center pixels average value for each band
-    center_pixels_radius = 20 
+    center_pixels_radius = 20
     xy_pixel_count = 410
     center_address = xy_pixel_count/2
-    expected_count = int(3.14*center_pixels_radius**2)
+    expected_count = int(3.14*center_pixels_radius**2)+1
 
     center_spot_average_per_band = []
     for z in range (0, z_count): 
@@ -39,10 +39,33 @@ def main():
                     value = input_cube[x][y][z]
                     spot_values.append( value )
         spot_average_value = np.average(spot_values)
-        print("band",z,":",wavelengths_nm[z],"nm","  spot value =", int(round(spot_average_value,0)), " Counted", center_pixels_count, "pixels, expected", expected_count)
+        if spot_average_value < 1: 
+            spot_average_value = 1
         center_spot_average_per_band.append(spot_average_value)
+        print("band",z,":",wavelengths_nm[z],"nm","  spot value =", int(round(spot_average_value,0)), " Counted", center_pixels_count, "pixels, expected", expected_count)
+        
+    print("generating flat field calibration cube...")
+    flat_field_correction_cube = np.zeros_like(input_cube,dtype=np.float64)
+    for z in range (0, z_count): 
+        print("generating flat field calibration for wavelength", wavelengths_nm[z],"nm")
+        for x in range(0, x_count):
+            for y in range(0, y_count):
+                cal_pixel_value = input_cube[x][y][z]
+                if cal_pixel_value > 0: 
+                    flat_field_correction_cube[x][y][z] = center_spot_average_per_band[z]/ cal_pixel_value
+                else: 
+                    flat_field_correction_cube[x][y][z] = 1
+    max_correction = np.max(flat_field_correction_cube)
+    min_correction = np.min(flat_field_correction_cube)
+    print("min =", min_correction, "max = ", max_correction)
 
-
+    output_folder = '../AIST_data_files/'
+    output_filename = 'ultris_ff_cal_p7int_3lamps_session_000_034.npy' 
+    try: 
+        np.save(Path(output_folder, output_filename), flat_field_correction_cube)
+        print("flat field correction cube file saved as:", output_filename)
+    except Exception as err: 
+        print("file not saved because ", err)
 
 
 
