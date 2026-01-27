c7cce0efe1db9bcefd90a89be09871d5acd36c2d
Paul Mirel <paul.mirel@startmail.com>
2026-01-27 16:30:25 -0500

    made plots from both sources

--- practice/make_plot_from_csv.py
+++ practice/make_plot_from_csv.py
@@ -1,18 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from pathlib import Path
+import csv
 
-# Data for plotting
-t = np.arange(0.0, 2.0, 0.01)
-s = 1 + np.sin(2 * np.pi * t)
-cs = 1 + np.cos(2 * np.pi * t)
+input_folder = '../AIST_data_files/'
+input_filename = 'Ar_intensity_v_WL.csv'
 
-fig, ax = plt.subplots()
-ax.plot(t, s)
-ax.plot(t, cs)
+with open(Path(input_folder, input_filename), newline='') as wl_intensity_pairs:
+    wl_tuples = list(csv.reader(wl_intensity_pairs))
 
-ax.set(xlabel='time (s)', ylabel='voltage (mV)',
-       title='About as simple as it gets, folks')
-ax.grid()
+wl = []
+rel = []
+for index in range (0, len(wl_tuples)):
+    wl.append(float(wl_tuples[index][0]))
+    rel.append(float(wl_tuples[index][1]))
 
-fig.savefig("test.png")
-plt.show()
+if True: 
+    fig, ax = plt.subplots()
+    ax.plot(wl, rel)
+
+    ax.set(xlabel='wavelength (nm)', ylabel='relative intensity',
+        title='NIST emission line spectrum: Ar')
+    ax.grid()
+
+    #fig.savefig("test.png")
+    plt.show()
